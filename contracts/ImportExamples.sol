3cae4f3e87e71b683210c3b3942dec8066360ba7
S0AndS0 <strangerthanbland@gmail.com>
2022-11-12 16:15:36 -0800

    Trick Truffle to expose artifacts to JavaScript

---
+++ contracts/ImportExamples.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+import { ExampleUsage } from "../test/solidity/lib/ExampleUsage.sol";
+import { NotOwnable } from "../test/solidity/lib/NotOwnable.sol";
+
+/// @title Trick Truffle to expose artifacts to JavaScript tests
+contract ImportExamples {}
