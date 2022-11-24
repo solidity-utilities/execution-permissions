c9723d1bfe4e0344081dceee4f5aa31ea3de474f
S0AndS0 <strangerthanbland@gmail.com>
2022-11-24 14:05:59 -0800

    :tada: Add type hints and doc-comments

--- ts/test/javascript/lib/web3-ganache-helpers.ts
+++ ts/test/javascript/lib/web3-ganache-helpers.ts
@@ -1,39 +1,85 @@
-"use strict";
+// vim: noexpandtab
+'use strict';
 
-//
-function promisedWeb3Send(dict) {
-  return new Promise((resolve, reject) => {
-    web3.currentProvider.send(dict, (error, result) => {
-      if (error) {
-        return reject(error);
-      }
-      return resolve(result);
-    });
-  });
+/**
+ * @file Ganache utility functions
+ * @author S0AndS0
+ *
+ * @example
+ * ```javascript
+ * contract("<Contract_Name>.<Function_Name>", (accounts) => {
+ *   let snapshot_id;
+ *
+ *   beforeEach(async () => {
+ *     snapshot_id = (await takeSnapShot()).result;
+ *   });
+ *
+ *   afterEach(async () => {
+ *     await revertToSnapShot(snapshot_id);
+ *   });
+ * });
+ * ```
+ */
+
+import { JsonRpcPayload, JsonRpcResponse } from 'web3-core-helpers';
+
+/**
+ * Transmute `web3.currentProvider.send` into `Promise`
+ */
+function promisedWeb3Send(dict: JsonRpcPayload): Promise<JsonRpcResponse | undefined> {
+	return new Promise((resolve, reject) => {
+		if (web3.currentProvider == null || typeof web3.currentProvider === 'string') {
+			return reject(new Error('promisedWeb3Send -> Missing `web3.currentProvider`'));
+		}
+
+		if (!web3.currentProvider.send) {
+			return reject(new Error('promisedWeb3Send -> Missing `web3.currentProvider.send`'));
+		}
+
+		if (!dict || !Object.keys(dict).length) {
+			return reject(new Error('promisedWeb3Send -> Missing `dict`'));
+		}
+
+		web3.currentProvider.send(dict, (error, result) => {
+			if (error) {
+				return reject(error);
+			}
+			return resolve(result);
+		});
+	});
 }
 
-//
-// @see {link} https://medium.com/fluidity/standing-the-time-of-test-b906fcc374a9
-function takeSnapShot() {
-  return promisedWeb3Send({
-    jsonrpc: "2.0",
-    method: "evm_snapshot",
-    id: new Date().getTime(),
-  });
+/**
+ * Save and return `.result` snapshot ID of blockchain state
+ * @see {@link https://medium.com/fluidity/standing-the-time-of-test-b906fcc374a9}
+ */
+function takeSnapShot(): Promise<JsonRpcResponse | undefined> {
+	return promisedWeb3Send({
+		jsonrpc: '2.0',
+		method: 'evm_snapshot',
+		id: new Date().getTime(),
+	});
 }
 
-//
-function revertToSnapShot(id) {
-  return promisedWeb3Send({
-    jsonrpc: "2.0",
-    method: "evm_revert",
-    params: [id],
-    id: new Date().getTime(),
-  });
+/**
+ * Restore blockchain state to given ID
+ * Warning: saved state of IDs greater than what is provided will be discarded
+ */
+function revertToSnapShot(id: JsonRpcPayload['id']): Promise<JsonRpcResponse | undefined> {
+	if (!id) {
+		return Promise.reject(new Error('revertToSnapShot -> Missing `id`'));
+	}
+
+	return promisedWeb3Send({
+		jsonrpc: '2.0',
+		method: 'evm_revert',
+		params: [id],
+		id: new Date().getTime(),
+	});
 }
 
 module.exports = {
-  promisedWeb3Send,
-  takeSnapShot,
-  revertToSnapShot,
+	promisedWeb3Send,
+	takeSnapShot,
+	revertToSnapShot,
 };
