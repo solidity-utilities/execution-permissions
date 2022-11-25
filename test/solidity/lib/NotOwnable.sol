6b211d1466f444fd686ec87c6985b1d5ea7cfa31
S0AndS0 <strangerthanbland@gmail.com>
2022-11-24 16:34:40 -0800

    :butter: Trick TypeChain into building type hints

--- test/solidity/lib/NotOwnable.sol
+++ test/solidity/lib/NotOwnable.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.4.22 <0.9.0;
 
-/// @title
+import { IExecutionPermissions, BatchPermissionEntry } from "../../../contracts/interfaces/IExecutionPermissions.sol";
+
+/// @title Contract used for testing error handling
 /// @author S0AndS0
 contract NotOwnable {
-
+    address public permissionStore;
 }
