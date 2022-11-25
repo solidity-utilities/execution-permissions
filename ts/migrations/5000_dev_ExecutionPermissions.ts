bbc0d2ea79fe0ad5085d9b05ed63957b835a4d39
S0AndS0 <strangerthanbland@gmail.com>
2022-11-25 09:24:28 -0800

    :tada: Add type hints and use Prettier

--- ts/migrations/5000_dev_ExecutionPermissions.ts
+++ ts/migrations/5000_dev_ExecutionPermissions.ts
@@ -1,19 +1,21 @@
-"use strict";
+// vim: noexpandtab
+/// <reference types="../../@types/truffle-v5/types" />
+'use strict';
 
-const path = require("path");
+import path from 'path';
 
-module.exports = (deployer, network, accounts) => {
-  if (network !== "development") {
-    const file_name = path.basename(__filename);
-    console.warn(`Skipped ${file_name} because network is`, network);
-    return;
-  }
+module.exports = (deployer: Truffle.Deployer, network: string, accounts: Truffle.Accounts) => {
+	if (network !== 'development') {
+		const file_name = path.basename(__filename);
+		console.warn(`Skipped ${file_name} because network is`, network);
+		return;
+	}
 
-  const ExecutionPermissions = artifacts.require("ExecutionPermissions");
+	const ExecutionPermissions = artifacts.require('ExecutionPermissions');
 
-  const parameters = {
-    // owner: accounts[0],
-  };
+	const parameters = {
+		// owner: accounts[0],
+	};
 
-  deployer.deploy(ExecutionPermissions, ...Object.values(parameters));
+	deployer.deploy(ExecutionPermissions, ...Object.values(parameters));
 };
