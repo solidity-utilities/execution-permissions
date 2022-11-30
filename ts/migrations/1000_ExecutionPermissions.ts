757324885ae6207bc49e8cc208a5c145cbad6b1d
S0AndS0 <strangerthanbland@gmail.com>
2022-11-29 21:27:00 -0800

    :warning: Allow any network to publish/migrate

--- ts/migrations/1000_ExecutionPermissions.ts
+++ ts/migrations/1000_ExecutionPermissions.ts
@@ -5,17 +5,9 @@
 import path from 'path';
 
 module.exports = (deployer: Truffle.Deployer, network: string, accounts: Truffle.Accounts) => {
-	if (network !== 'development') {
-		const file_name = path.basename(__filename);
-		console.warn(`Skipped ${file_name} because network is`, network);
-		return;
-	}
-
 	const ExecutionPermissions = artifacts.require('ExecutionPermissions');
 
-	const parameters = {
-		// owner: accounts[0],
-	};
+	const parameters = {};
 
 	deployer.deploy(ExecutionPermissions, ...Object.values(parameters));
 };
